59ca65fe1496b0b83f1f586836e54e2bf507de44
Canaan <canaanshi@outlook.com>
2021-07-17 21:19:59 +0800

    complete 7.Reverse Integer.java and README.md

---
+++ algorithms/0007.Reverse-Integer.java
@@ -0,0 +1,65 @@
+/*
+ * @lc app=leetcode id=7 lang=java
+ *
+ * [7] Reverse Integer
+ *
+ * https://leetcode.com/problems/reverse-integer/description/
+ *
+ * algorithms
+ * Easy (26.07%)
+ * Likes:    5165
+ * Dislikes: 7786
+ * Total Accepted:    1.6M
+ * Total Submissions: 6.3M
+ * Testcase Example:  '123'
+ *
+ * Given a signed 32-bit integer x, return x with its digits reversed. If
+ * reversing x causes the value to go outside the signed 32-bit integer range
+ * [-2^31, 2^31 - 1], then return 0.
+ *
+ * Assume the environment does not allow you to store 64-bit integers (signed
+ * or unsigned).
+ *
+ *
+ * Example 1:
+ * Input: x = 123
+ * Output: 321
+ * Example 2:
+ * Input: x = -123
+ * Output: -321
+ * Example 3:
+ * Input: x = 120
+ * Output: 21
+ * Example 4:
+ * Input: x = 0
+ * Output: 0
+ *
+ *
+ * Constraints:
+ *
+ *
+ * -2^31 <= x <= 2^31 - 1
+ *
+ *
+ */
+
+class Solution {
+  public int reverse(int x) {
+    int ans = 0;
+    while (x != 0) {
+      // take the last number
+      int tail = x % 10;
+      // if ans * 10 + tail > MAX_VALUE, out of memory
+      if (ans > Integer.MAX_VALUE || (ans == Integer.MAX_VALUE && tail > 7)) {
+        return 0;
+      }
+      // if ans * 10 + tail < Min_VALUE, out of memory
+      if (ans < Integer.MIN_VALUE || (ans == Integer.MIN_VALUE && tail < -8)) {
+        return 0;
+      }
+      ans = ans * 10 + tail;
+      x /= 10;
+    }
+    return ans;
+  }
+}
